415a53365893e9c877f899623126ba8146a7fd1f
remnantdochi <44366193+remnantdochi@users.noreply.github.com>
2018-11-12 12:35:12 +0900

    4일차 7차시 linked list
    여전히 시간 부족...

--- test4-2.py
+++ test4-2.py
@@ -1,13 +1,28 @@
+class node:
+    def __init__(self,data,next):
+        self.data=data
+        self.next=next
+def findindex(sourceList,index):
+    i=0
+    tmpnode=sourceList
+    while i != index:
+        tmpnode=tmpnode.next
+        i+=1
+    return tmpnode
 def partition(A,l,r):
-    p=A[l]
+    p=findindex(A,l).data
     i=l+1
     j=r
     while i<=j:
-        while (i<=j and A[i]<=p) : i+=1
-        while (i<=j and A[j]>=p) : j-=1
+        while (i<=j and findindex(A,i).data<=p) : i+=1
+        while (i<=j and findindex(A,j).data>=p) : j-=1
         if i<=j:
-            A[i],A[j]=A[j],A[i]
-    A[l],A[j]=A[j],A[l]
+            idata=findindex(A,i).data
+            jdata=findindex(A,j).data
+            findindex(A,i).data,findindex(A,j).data=jdata,idata
+    ldata=findindex(A,l).data
+    jdata=findindex(A,j).data
+    findindex(A,l).data,findindex(A,j).data=jdata,ldata
     return j
 def quicksort(A,l,r):
     if l<r:
@@ -18,5 +33,12 @@ T=int(input())
 for test_case in range(1,T+1):
     N=int(input())
     todolist=list(map(int,input().split()))
-    quicksort(todolist,0,N-1)
-    print("#{} {}".format(test_case,todolist[N//2]))
+    for i in range(N):
+        if i==0:
+            Head=node(todolist[0],None)
+            tmpnode=Head
+        else:
+            tmpnode.next=node(todolist[i],None)
+            tmpnode=tmpnode.next
+    quicksort(Head,0,N-1)
+    print("#{} {}".format(test_case,findindex(Head,N//2).data))
