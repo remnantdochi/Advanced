742f9784a20f9064478e8180f53dd621c2f7dd98
remnantdochi <44366193+remnantdochi@users.noreply.github.com>
2018-11-12 11:58:16 +0900

    4일차 6차시 linked list
    여전히 제한시간 부족...

--- test4-1.py
+++ test4-1.py
@@ -1,25 +1,90 @@
-def merge(left,right):
+class Node:
+    def __init__(self,data,next):
+        self.data=data
+        self.next=next
+'''
+def printlist(sourceList):
+    printnode=sourceList
+    if sourceList==None:
+        print('printnode-None')
+    else:
+        while printnode.next != None:
+            print('printnode',printnode.data,end=" ")
+            printnode=printnode.next
+        print('printnode',printnode.data)'''
+def findtail(sourceList):
+    tmpnode=sourceList
+    while tmpnode.next != None:
+        tmpnode=tmpnode.next
+    return tmpnode.data
+def merge(leftHalf, rightHalf):
     global cnt
-    if left[len(left)-1] > right[len(right)-1]:
+    if findtail(leftHalf) > findtail(rightHalf):
         cnt+=1
-    result=[]
-    while len(left)>0 and len(right)>0:
-        if left[0] <= right[0]: result.append(left.pop(0))
-        else : result.append(right.pop(0))
-    if len(left)>0: result.extend(left)
-    if len(right)>0: result.extend(right)
-    return result
+    fake_head = Node(None,None)
+    curr = fake_head
+    while leftHalf and rightHalf:
+        if leftHalf.data < rightHalf.data:
+            curr.next = leftHalf
+            leftHalf = leftHalf.next
+        else:
+            curr.next = rightHalf
+            rightHalf = rightHalf.next
+        curr = curr.next
+    if leftHalf == None:
+        curr.next = rightHalf
+    elif rightHalf == None:
+        curr.next = leftHalf
+    return fake_head.next
+def countlist(sourceList):
+    count=1
+    tmpnode=sourceList
+    while tmpnode.next != None:
+        tmpnode=tmpnode.next
+        count+=1
+    return count
+def split(sourceList):
+    if sourceList == None or sourceList.next == None:
+        leftHalf = sourceList
+        rightHalf = None
+        return leftHalf, rightHalf
+    else:
+        numofdata=countlist(sourceList)
+        count=0
+        midPointer = sourceList
+        while count != numofdata//2-1:
+            midPointer=midPointer.next
+            count += 1
+    leftHalf = sourceList
+    rightHalf = midPointer.next
+    midPointer.next = None
+    return leftHalf, rightHalf
 def merge_sort(m):
-    if len(m) <= 1: return m
-    left=m[:len(m)//2]
-    right=m[len(m)//2:]
+    if m==None or m.next==None:
+        return m
+    left,right= split(m)
     left=merge_sort(left)
     right=merge_sort(right)
     return merge(left,right)
+def findmiddle(sourceList,N):
+    tmpnode=sourceList
+    length=0
+    while length != N//2:
+        tmpnode=tmpnode.next
+        length+=1
+    return tmpnode.data
 T=int(input())
 for test_case in range(1,T+1):
     N=int(input())
     todolist=list(map(int,input().split()))
+    for i in range(N):
+        if i==0:
+            Head=Node(todolist[0],None)
+            tmpnode=Head
+        else:
+            tmpnode.next=Node(todolist[i],None)
+            tmpnode=tmpnode.next
     cnt=0
-    result=merge_sort(todolist)
-    print("#{} {} {}".format(test_case,result[N//2],cnt))
+    resultnode=merge_sort(Head)
+    result=findmiddle(resultnode,N)
+    print("#{} {} {}".format(test_case,result,cnt))
